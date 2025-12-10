7a953af9c78b487dd71996613736c8ac4e05f2a2
HONOR <sergeychirkov24@gmail.com>
2025-12-10 03:10:52 +0300

    Добавлена имитация инкапсуляции методов классов

--- class.c
+++ class.c
@@ -6,43 +6,80 @@
 #include "class.h"
 
 // Class Person
-// конструктор(передача значений пользователя в поля)
-void init_user(Person* User, int age, const char* name){
-    User -> age = age;
+static void person_print(Person* self) {
+    printf("name: %s, age: %d\n",
+        self->name ? self->name : "no name", // захотел красивенько вывести
+        self->age);
+}
 
-    // динам. выделение памяти на имя юзера
-    User -> name = (char*)malloc(strlen(name) + 1); // +1 тк \0 не уитывается
-    if (User -> name != NULL){
-        User -> name = strcpy(User -> name, name);
+static void person_set_age(Person* self, int new_age) {
+    self->age = new_age;
+}
+
+// фабрика класса Person
+Person* person_create(int age, const char* name) {
+    Person* p = (Person*)malloc(sizeof(Person));
+    if (!p) {   // чек выделения памяти
+        return NULL;
     }
-}
 
-// метод вывода значений(чисто принт)
-void print_user(Person* User){
-    printf("Name: %s, Age: %d\n", User -> name, User -> age);
-}
+    p->age = age;
 
-// деструктор
-void destroy_user(Person* User){
-    if (User -> name != NULL){
-        free(User -> name);
-        User -> name = NULL;
+    if (name) {
+        p->name = (char*)malloc(sizeof(name) + 1); // тк \o или /o отдельный символ
+        if (!(p->name)) {   // чек выделения памяти
+            free(p);
+            return NULL;
+        }
+        strcpy(p->name, name);
+    } else {
+        p->name = NULL;
     }
+
+    // привязка методов
+    p->print = person_print;
+    p->set_age = person_set_age;
+
+    return p;
 }
 
 
-// Class Rectangle
-void init_rect(Rectangle* Rect, double len, double wid){
-    Rect -> len = len;
-    Rect -> wid = wid;
+// методы Rectangle
+static void rectangle_print(Rectangle* self) {
+    printf("Rectangle -> len: %lf, wid: %lf\n", self->len, self->wid);
 }
 
-void print_rect_info(Rectangle* Rect){
-    printf("Length: %lf, Width: %lf\n", Rect -> len, Rect -> wid);
+static double rectangle_area(Rectangle* self) {
+    double s = (self->len) * (self->wid);
+
+    return s;
 }
 
-double print_rect_area(Rectangle* Rect){
-    double s = (Rect -> len) * (Rect -> wid);
+// фабрика класса Rectangle
+Rectangle* rectangle_create(double len, double wid) {
+    Rectangle* r = (Rectangle*)malloc(sizeof(Rectangle));
+    if (!r) {
+        return NULL;
+    }
 
-    printf("Area of rectangle: %lf\n", s);
+    r->len = len;
+    r->wid = wid;
+    r->area = rectangle_area;
+    r->print = rectangle_print;
+
+    return r;
+}
+
+// деструкторы классов
+void person_destroy(Person* p) {
+    if (!p) return;
+
+    free(p->name);
+    free(p);
+}
+
+void rectangle_destroy(Rectangle* r) {
+    if (!r) return;
+
+    free(r);
 }
